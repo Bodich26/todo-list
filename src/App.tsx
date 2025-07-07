3ac18435b05c31492185b4292857dd14caea7bcf
Bogdan <4634815@gmai.com>
2025-07-07 14:49:57 +0300

    create header and footer widgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App";
+import { Footer, Header } from "./widgets";
 
 function App() {
   return (
-    <>
-      <h1>Montserrat</h1>
-    </>
+    <div className="flex flex-col relative min-h-screen bg-section">
+      <Header />
+      <main className="flex-1">
+        <h1>Montserrat</h1>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/widgets/footer/ui/footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <footer>©Copyright Bodich 2025</footer>;
+};
